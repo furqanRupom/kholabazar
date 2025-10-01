b7cd4f48860916a3b72b5f0b445e40fd26c8471f
furqanrupom <furqanrupom978@gmail.com>
2025-10-01 21:09:06 +0600

    feat(router): config advanced routing in go

---
+++ kholabazar/backend/handlers/create_product.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"kholabazar/database"
+	"kholabazar/utils"
+)
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(200)
+		return
+	}
+	var newProduct database.Product
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newProduct)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Please give me valid JSON", 400)
+		return
+	}
+	newProduct.ID = len(database.ProductList) + 1
+	database.ProductList = append(database.ProductList, newProduct)
+	utils.SendData(w, newProduct, 201)
+}
