26147eace55dadb31fde4d9b82c5a4d89238babc
furqanrupom <furqanrupom978@gmail.com>
2025-10-03 22:11:05 +0600

    feat : we made createJWT utils absolute from scratch in go

--- kholabazar/backend/main.go
+++ kholabazar/backend/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"kholabazar/cmd"
+	"fmt"
+	"kholabazar/utils"
 )
 
 func main() {
-	cmd.Serve()
+	user := utils.Payload{
+		Sub: 1,
+		FirstName: "Furqan",
+		Email: "furqanrupom978@gmail.com",
+		LastName: "Ahmad",
+		IsShopOwner: false,
+	}
+	jwt,err := utils.CreateJWT(user,"my-secret")
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+	fmt.Println(jwt)
+
+	// cmd.Serve()
 }
